53ef40736064c59697b22a6ebc4240498cc92e3f
frank <frank@vollenweider.info>
2013-04-19 18:30:51 +0200

    implemented filtering

--- modules/ExpenseList/js/expenselist.js
+++ modules/ExpenseList/js/expenselist.js
@@ -1,37 +1,25 @@
 (function($) {
 
-	"use strict";
-
-	/**
-     * ExpenseList module implementation.
-     *
-     * @author Frank Vollenweider <frank@vollenweider.info>
-     * @namespace Tc.Module
-     * @class ExpenseList
-     * @extends Tc.Module
-     */
     Tc.Module.ExpenseList = Tc.Module.extend({
 
-    	// Template-Cache
-        templatesCache: [],
+        // Template-Cache
+        templatesCache: {},
 
         // Data-Cache
-        estatesCache: [],
-        propertiesCache: [],
-        expenseCategoriesCache: [],
-        expensesCache: [],
+        estatesCache: {},
+        propertiesCache: {},
+        expenseCategoriesCache: {},
+        expensesCache: {},
 
         on: function(callback) {
 
             var that = this;
             var $ctx = this.$ctx;
 
-            this.sandbox.subscribe(1, this); // FilterData
+            this.sandbox.subscribe(1, this); // Filter
 
             // compile templates
             this.templatesCache['expense'] = doT.template($('#expense').text());
-
-            console.log("expenselist.js: end of on");
 
             callback();
         },
@@ -54,50 +42,83 @@
                 data: JSON.stringify(data),
                 dataType: 'json',
                 success: function(data) {
+
                     if (!data.success) { that.onError(data.messages); return; };
 
-                    if (data.estates) {
-                    	that.addToCache(data.estates, that.estatesCache);
-                    }
+                    that.addToCache(data.estates, that.estatesCache);
 
-                    if (data.properties) {
-                    	that.addToCache(data.properties, that.propertiesCache);
-                    }
+                    that.addToCache(data.properties, that.propertiesCache);
 
-                    if (data.expenseCategories) {
-                        that.addToCache(data.expenseCategories, that.expenseCategoriesCache);
-                    }
+                    that.addToCache(data.expenseCategories, that.expenseCategoriesCache);
 
-                    if (data.expenses) {
-                        that.addToCache(data.expenses, that.expensesCache);
-                    	var $table = $('table', $ctx);
-                    	$.each(data.expenses, function(key, expense) {
-                    		expense.category["title"] = that.expenseCategoriesCache[expense.category.id].title;
-                    		var newExpense = $(that.templatesCache['expense'](expense));
-                    		$('table', $ctx).append(newExpense);
-                    	});
-                    }
+                    that.renderExpenses(data.expenses);
 
                     var filterData = {
                         estates : that.estatesCache,
                         properties : that.propertiesCache,
                         categories : that.expenseCategoriesCache
                     };
-                    console.log("expenselist.js: end of after (bevor fire....)");
-                    that.fire('filterDataLoaded', filterData, [1]); // FilterData
+
+                    that.fire('filterDataLoaded', filterData, [1]); // Filter
 
                 }
             });
-			console.log("expenselist.js: End of load");
 
         },
 
         addToCache: function(items, cache) {
-        	if (items) {
-        		$.each(items, function(key, item) {
-                	cache[item.id] = item;
+            if (items) {
+                $.each(items, function(key, item) {
+                    cache[item.id] = item;
                 });
-        	}
+            }
+        },
+
+        onFilterChanged: function(filterValues) {
+
+            var that = this;
+            var $ctx = this.$ctx;
+
+            var data = {
+                "filterValues": {
+                	"estateId": filterValues.estateId,
+                	"propertyId": filterValues.propertyId,
+                	"categoryId": filterValues.categoryId
+                }
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: serviceUrl('expenses/filter'),
+                data: JSON.stringify(data),
+                dataType: 'json',
+                success: function(data) {
+
+                    if (!data.success) { that.onError(data.messages); return; };
+
+                    that.renderExpenses(data.expenses);
+
+                }
+            });
+        },
+
+        renderExpenses: function(expenses) {
+
+            var that = this;
+            var $ctx = this.$ctx;
+
+            if (expenses) {
+            	that.expensesCache = {};
+                that.addToCache(expenses, that.expensesCache);
+                var $table = $('table', $ctx);
+                $table.empty();
+                $.each(expenses, function(key, expense) {
+                    expense.category["title"] = that.expenseCategoriesCache[expense.category.id].title;
+                    var newExpense = $(that.templatesCache['expense'](expense));
+                    $('table', $ctx).append(newExpense);
+                });
+            }
+
         },
 
         onError: function(messages) {
@@ -110,4 +131,5 @@
         }
 
     });
+
 })(Tc.$);
--- modules/Filter/js/filter.js
+++ modules/Filter/js/filter.js
@@ -1,33 +1,30 @@
 (function($) {
-    /**
-     * ExpenseList module implementation.
-     *
-     * @author Frank Vollenweider <frank@vollenweider.info>
-     * @namespace Tc.Module
-     * @class Filter
-     * @extends Tc.Module
-     */
+
     Tc.Module.Filter = Tc.Module.extend({
 
-    	// Template-Cache
+        // Template-Cache
         templatesCache: [],
 
-    	on: function(callback) {
+        on: function(callback) {
 
             var that = this;
             var $ctx = this.$ctx;
 
-            this.sandbox.subscribe(1, this); // FilterData
+            this.sandbox.subscribe(1, this); // Filter
 
             // compile templates
             this.templatesCache['option'] = doT.template($('#option').text());
-			console.log("filter.js: End of load");
+
+            // bind filter change action
+            this.$ctx.on('change', '.filter select', function() {
+                that.filterChanged();
+            });
 
             callback();
         },
         
         after: function() {
-			console.log("filter.js: End of after");
+            // NOOP
         },
 
         onFilterDataLoaded: function(data) {
@@ -35,31 +32,37 @@
             var $ctx = this.$ctx;
             var that = this;
 
-            console.log(data.estates);
-        	if (data.estates) {
-        		$.each(data.estates, function(key, item) {
-                    if (item) {
-            			$('#fld-liegenschaft', $ctx).append($(that.templatesCache['option'](item)));
-                    }
+            if (data.estates) {
+                $.each(data.estates, function(key, item) {
+                    $('#fld-liegenschaft', $ctx).append($(that.templatesCache['option'](item)));
                 });
-        	}
-			console.log("filter.js: onFilterDataLoaded - " + data.properties);
-        	if (data.properties) {
-        		$.each(data.properties, function(key, item) {
-                	if (item) {
-            			$('#fld-objekt', $ctx).append($(that.templatesCache['option'](item)));
-                    }
+            }
+            if (data.properties) {
+                $.each(data.properties, function(key, item) {
+                    $('#fld-objekt', $ctx).append($(that.templatesCache['option'](item)));
                 });
-        	}
-			console.log("filter.js: onFilterDataLoaded - " + data.categories);
-        	if (data.categories) {
-        		$.each(data.categories, function(key, item) {
-                	if (item) {
-            			$('#fld-ausgabentyp', $ctx).append($(that.templatesCache['option'](item)));
-                    }
+            }
+            if (data.categories) {
+                $.each(data.categories, function(key, item) {
+                    $('#fld-ausgabentyp', $ctx).append($(that.templatesCache['option'](item)));
                 });
-        	}
+            }
+        },
+
+        filterChanged: function() {
+            
+            var $ctx = this.$ctx;
+            var that = this;
+
+            var filterValues = {
+                estateId : $('#fld-liegenschaft', $ctx).val(),
+                propertyId : $('#fld-objekt', $ctx).val(),
+                categoryId : $('#fld-ausgabentyp', $ctx).val()
+            };
+
+            that.fire('filterChanged', filterValues, [1]); // Filter
         }
 
     });
+
 })(Tc.$);
