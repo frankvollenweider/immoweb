02e0a0bffcbe4d85783128678c8e5c2c504c03e3
frank <frank@vollenweider.info>
2013-04-19 07:54:37 +0200

    load expenses test data

--- modules/ExpenseList/js/expenselist.js
+++ modules/ExpenseList/js/expenselist.js
@@ -1,38 +1,117 @@
 (function($) {
-    /**
+
+	"use strict";
+
+	/**
      * ExpenseList module implementation.
      *
-     * @author Roger Dudler <roger.dudler@namics.com>
+     * @author Frank Vollenweider <frank@vollenweider.info>
      * @namespace Tc.Module
      * @class ExpenseList
      * @extends Tc.Module
      */
     Tc.Module.ExpenseList = Tc.Module.extend({
-        
+
+    	// Template-Cache
+        templatesCache: [],
+
+        // Data-Cache
+        estatesCache: [],
+        propertiesCache: [],
+        expenseCategoriesCache: [],
+        expensesCache: [],
+
         on: function(callback) {
+
             var that = this;
             var $ctx = this.$ctx;
 
-            var data = {
-                'title': 'fjklasdjfskfsd'
-            };
-            var template = doT.template($('script', $ctx).text());
-            
-            var row = $(template(data));
-            $('table', $ctx).append(row);
-            row = $(template(data));
-            $('table', $ctx).append(row);
-            row = $(template(data));
-            $('table', $ctx).append(row);
-            row = $(template(data));
-            $('table', $ctx).append(row);
+            this.sandbox.subscribe(1, this); // FilterData
 
+            // compile templates
+            this.templatesCache['expense'] = doT.template($('#expense').text());
+
+            // initial load
+            this.load();
+            console.log("expenselist.js: end of on");
 
             callback();
         },
         
         after: function() {
-            
+
+            var that = this;
+            var $ctx = this.$ctx;
+
+            var filterData = {
+                estates : that.estatesCache,
+                properties : that.propertiesCache,
+                categories : that.expenseCategoriesCache
+            };
+            console.log("expenselist.js: end of after (bevor fire....)");
+            that.fire('filterDataLoaded', filterData, [1]); // FilterData
+
+        },
+
+        load: function() {
+
+            var that = this;
+            var $ctx = this.$ctx;
+
+            var data = {};
+
+            $.ajax({
+                type: 'POST',
+                url: serviceUrl('expenses/load'),
+                data: JSON.stringify(data),
+                dataType: 'json',
+                success: function(data) {
+
+                    if (!data.success) { that.onError(data.messages); return; };
+
+                    if (data.estates) {
+                    	that.addToCache(data.estates, that.estatesCache);
+                    }
+
+                    if (data.properties) {
+                    	that.addToCache(data.properties, that.propertiesCache);
+                    }
+
+                    if (data.expenseCategories) {
+                        that.addToCache(data.expenseCategories, that.expenseCategoriesCache);
+                    }
+
+                    if (data.expenses) {
+                        that.addToCache(data.expenses, that.expensesCache);
+                    	var $table = $('table', $ctx);
+                    	$.each(data.expenses, function(key, expense) {
+                    		expense.category["title"] = that.expenseCategoriesCache[expense.category.id].title;
+                    		var newExpense = $(that.templatesCache['expense'](expense));
+                    		$('table', $ctx).append(newExpense);
+                    	});
+                    }
+
+                }
+            });
+			console.log("expenselist.js: End of load");
+
+        },
+
+        addToCache: function(items, cache) {
+        	if (items) {
+        		$.each(items, function(key, item) {
+                	cache[item.id] = item;
+                });
+        	}
+        },
+
+        onError: function(messages) {
+            var $messages = $('.messages');
+            $.each(messages, function(key, message) {
+                var $msg = $('<div class="msg msg-' + message.type + '">' + message.message + '</div>');
+                $messages.append($msg);
+                $msg.fadeIn('fast');
+            });
         }
 
     });
